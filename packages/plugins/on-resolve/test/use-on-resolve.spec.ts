7febdc0f46c7da2975f16f27a7eba2e842f95939
Cecchi MacNaughton <cecchi.macnaughton@gmail.com>
2025-11-01 09:44:50 -0700

    feat[useOnResolve]: add skipDefaultResolvers option

--- packages/plugins/on-resolve/test/use-on-resolve.spec.ts
+++ packages/plugins/on-resolve/test/use-on-resolve.spec.ts
@@ -8,12 +8,20 @@ describe('useOnResolve', () => {
       type Query {
         value1: String!
         value2: String!
+        obj: Obj!
+      }
+
+      type Obj {
+        field1: String!
       }
     `,
     resolvers: {
       Query: {
         value1: () => 'value1',
         value2: () => 'value2',
+        obj: () => ({
+          field1: 'field1',
+        }),
       },
     },
   });
@@ -83,6 +91,47 @@ describe('useOnResolve', () => {
 
     expect(onResolveFn).toHaveBeenCalledTimes(0);
     expect(onResolveDoneFn).toHaveBeenCalledTimes(0);
+  });
+
+  it('should invoke the callback for default resolvers when not skipping', async () => {
+    const onResolveDoneFn = jest.fn();
+    const onResolveFn = jest.fn((_opts: OnResolveOptions) => onResolveDoneFn);
+    const testkit = createTestkit(
+      [useOnResolve(onResolveFn, { skipDefaultResolvers: false })],
+      schema,
+    );
+
+    await testkit.execute('{ obj { field1 } }');
+
+    expect(onResolveFn).toHaveBeenCalledTimes(2);
+    expect(onResolveDoneFn).toHaveBeenCalledTimes(2);
+
+    let i = 0;
+    for (const field of ['obj', 'field1']) {
+      expect(onResolveFn.mock.calls[i][0].context).toBeDefined();
+      expect(onResolveFn.mock.calls[i][0].root).toBeDefined();
+      expect(onResolveFn.mock.calls[i][0].args).toBeDefined();
+      expect(onResolveFn.mock.calls[i][0].info).toBeDefined();
+      expect(onResolveFn.mock.calls[i][0].info.fieldName).toBe(field);
+      expect(onResolveFn.mock.calls[i][0].resolver).toBeInstanceOf(Function);
+      expect(onResolveFn.mock.calls[i][0].replaceResolver).toBeInstanceOf(Function);
+
+      i++;
+    }
+  });
+
+  it('should not invoke the callback for default resolvers when skipping', async () => {
+    const onResolveDoneFn = jest.fn();
+    const onResolveFn = jest.fn((_opts: OnResolveOptions) => onResolveDoneFn);
+    const testkit = createTestkit(
+      [useOnResolve(onResolveFn, { skipDefaultResolvers: true })],
+      schema,
+    );
+
+    await testkit.execute('{ obj { field1 } }');
+
+    expect(onResolveFn).toHaveBeenCalledTimes(1);
+    expect(onResolveDoneFn).toHaveBeenCalledTimes(1);
   });
 
   it('should replace the result using the after hook', async () => {
