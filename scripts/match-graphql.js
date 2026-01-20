6d6a496b7a518ed85a7ce7c43c610183dabe24ca
Denis Badurina <denis@denelop.com>
2026-01-20 17:52:50 +0100

    patch graphql-modules types for grpahql v15

--- scripts/match-graphql.js
+++ scripts/match-graphql.js
@@ -1,4 +1,4 @@
-const { writeFileSync } = require('fs');
+const { writeFileSync, readFileSync } = require('fs');
 const { resolve } = require('path');
 const { argv, cwd } = require('process');
 
@@ -19,3 +19,18 @@ pkg.pnpm.overrides.graphql = npmVersion;
 pkg.devDependencies.graphql = npmVersion;
 
 writeFileSync(pkgPath, JSON.stringify(pkg, null, 2), 'utf8');
+
+// fix the graphql-modules plugin because the modules expectes a different signature on the execute
+// and this would be a specific issue to graphql-modules and not envelop since we're using a peer graphql
+if (version.startsWith('15.')) {
+  console.log('Patching graphql-modules plugin for GraphQL v15...');
+  const modulesPluginPath = resolve(cwd(), './packages/plugins/graphql-modules/src/index.ts');
+
+  const modulesPluginSource = readFileSync(modulesPluginPath, 'utf8');
+  const lines = modulesPluginSource.split('\n');
+
+  lines.splice(11, 0, '// @ts-expect-error only for graphql v15, revert when upgrading to v16');
+  lines.splice(19, 0, '// @ts-expect-error only for graphql v15, revert when upgrading to v16');
+
+  writeFileSync(modulesPluginPath, lines.join('\n'), 'utf8');
+}
